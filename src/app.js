7a020984d0bda64673ad25bd1ae8186dacf26f09
Aaron Cronin <ac6296@nyu.edu>
2020-04-20 09:45:57 -0400

    again

--- src/app.js
+++ src/app.js
@@ -40,9 +40,9 @@ app.use(function (req, res, next) {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("myapp/build"));
 
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.join(__dirname, "myapp", "build", "index.html"));
-  // });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "myapp", "build", "index.html"));
+  });
 }
 
 app.use("/routes", routes);
