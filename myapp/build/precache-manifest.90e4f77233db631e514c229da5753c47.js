86ff954fac50d3b4f1fdd781e5b7a272b9c80edd
Aaron Cronin <ac6296@nyu.edu>
2020-04-20 12:42:24 -0400

    build

---
+++ myapp/build/precache-manifest.90e4f77233db631e514c229da5753c47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "371938db5438b5248a5375fe6c8e8865",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62b895de6585b94885a9",
+    "url": "/static/css/main.006e9807.chunk.css"
+  },
+  {
+    "revision": "daa90bdbbb33a6d44224",
+    "url": "/static/js/2.e6e08172.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e6e08172.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62b895de6585b94885a9",
+    "url": "/static/js/main.c24d2389.chunk.js"
+  },
+  {
+    "revision": "bcb3e891f8c02b6d3b30",
+    "url": "/static/js/runtime-main.2d5c5ce2.js"
+  },
+  {
+    "revision": "567b5970364b0b4457fcaffe5c382d89",
+    "url": "/static/media/cocktail.567b5970.png"
+  }
+]);
