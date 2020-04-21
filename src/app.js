43a1398d8bcda67d685defb4486f640e1b02d702
Aaron Cronin <ac6296@nyu.edu>
2020-04-21 17:36:22 -0400

    udpate

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.use(function (req, res, next) {
   }
   next();
 });
-app.use("/", routes);
+app.use("/user", routes);
 
 app.use(express.static(path.join(__dirname, "..", "myapp", "build")));
 
